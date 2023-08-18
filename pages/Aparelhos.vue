bda765ef9451101635a615ea18f2048882e3fcd0
Henrique Lopes <hpereira@concent.com.br>
2023-08-18 18:04:27 -0300

    feat: adicionando novas funcionalidades a tabela

--- pages/Aparelhos.vue
+++ pages/Aparelhos.vue
@@ -1,8 +1,34 @@
 <template>
-    <div>
-      <div class="w-full">
-        <h1 class="font-bold text-3xl text-green-600">Aparelhos</h1>
-      </div>    
+  <div>
+    <div class="w-full flex gap-3 items-center">
+      <h1 class="font-bold text-3xl">Aparelhos</h1>
+      <button class="bg-green-600 text-white p-3 rounded-3xl hover:bg-white hover:text-green-600 hover:shadow-2xl" @click="showModal = true">Cadastrar</button>
     </div>
-  
-  </template>
+    <Modal v-show="showModal" @close-modal="showModal = false" />
+
+    <!-- <div class="save-btn">
+      <button @click="showModal = true">Save</button>
+    </div> -->
+
+    <div class="p-8">
+      <Table />
+    </div>
+  </div>
+
+</template>
+
+<script>
+import Modal from '~/components/Modal.vue'
+
+export default {
+  components: { Modal },
+  data() {
+    return {
+      showModal: false,
+    }
+  },
+}
+</script>
+
+<style>
+</style>
